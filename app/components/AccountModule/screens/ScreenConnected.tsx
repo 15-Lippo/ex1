4878d595eff54ea1a4f747b95f2d2d447ef96258
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-11-17 01:21:52 +0100

    Update ScreenConnected.tsx

--- app/components/AccountModule/screens/ScreenConnected.tsx
+++ app/components/AccountModule/screens/ScreenConnected.tsx
@@ -8,7 +8,7 @@ import {
   RADIUS,
   textStyle,
   useTheme,
-} from "@blossom-labs/rosette-ui";
+} from "@lisprocoin-labs/rosette-ui";
 import { useCallback, useEffect } from "react";
 import styled from "styled-components";
 import { useAccount, useNetwork } from "wagmi";
